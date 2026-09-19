5ced497096d739a0f97c5b3c8e10c5da408f8a29
SAIVINAY983 <billakantisaivinay943@gmail.com>
2026-09-19 14:36:33 +0530

    Initial commit for GateVault project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'gatevault'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
